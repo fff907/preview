75832d69d2abd6ed86a0c92897aaa1a95bd6226c
fff907 <59164817+fff907@users.noreply.github.com>
2025-03-06 15:56:06 +0900

    push

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -1,6 +1,8 @@
-gsap.to(".cover-video", {
-    opacity: 1,  // 透明度を 0 → 1 に変化
-    duration: 2, // 2秒かけて
-    ease: "power2.out" // 滑らかに変化
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.to(".cover-video", {
+      opacity: 1,  
+      duration: 2, 
+      ease: "power2.inOut"
+    });
   });
   
