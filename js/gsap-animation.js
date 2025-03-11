d48f9b27fd6dad61c8622c5056178f3de92c4ef8
fff907 <59164817+fff907@users.noreply.github.com>
2025-03-11 14:54:21 +0900

    a

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -1,5 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.onload = function () {
   const loadingScreen = document.querySelector(".loading");
+
 
   loadingScreen.style.display = "flex";
   loadingScreen.style.opacity = "1";
@@ -15,16 +16,15 @@ document.addEventListener("DOMContentLoaded", function () {
     opacity: 0,
     y: -20,
     duration: 0.5
-  }, "-=0.5") // ローディングバーが終わる直前にテキストをフェードアウト
+  }, "-=0.5")
   .to(".loading", {
     opacity: 0,
-    duration: 0.8, // 少し長めにフェードアウト
+    duration: 0.8,
     ease: "power2.inOut"
   })
   .add(() => {
-    // **アニメーションが完全に終わるまで display: none にしない**
     setTimeout(() => {
       loadingScreen.style.display = "none";
-    }, 300); // 少し遅延を入れる（0.3秒）
+    }, 300);
   });
-});
+};
