4446e2e64aea79d9aeb5467c1ad1f352e5290e86
fff907 <59164817+fff907@users.noreply.github.com>
2025-03-11 16:58:24 +0900

    a

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -1,19 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   const loading = document.querySelector(".loading");
 
-  // ローディングを確実に表示
   loading.style.display = "block";
   loading.style.opacity = "1";
 
   const tl = gsap.timeline();
 
-  // ローディングをフェードアウト
   tl.to(".loading", {
     opacity: 0,
     duration: 1,
     ease: "power2.inOut",
     onComplete: function () {
-      loading.style.display = "none"; // ローディングを完全に消す
+      loading.style.display = "none";
     }
   });
 });
