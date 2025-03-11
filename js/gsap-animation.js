aa47416a3a24e13edb27fba46b1e85331ade5c90
fff907 <59164817+fff907@users.noreply.github.com>
2025-03-11 15:48:55 +0900

    a

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -1,29 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const loading = document.querySelector(".loading");
-  const video = document.querySelector(".cover-video");
   loading.style.display = "flex";
 
   const tl = gsap.timeline();
 
-  if (video) {
-    video.addEventListener("loadeddata", function () {
-      tl.to(".loading", {
-        opacity: 0,
-        duration: 1,
-        ease: "power2.inOut",
-        onComplete: function () {
-          loading.style.display = "none";
-        }
-      });
-    });
-  } else {
-    tl.to(".loading", {
-      opacity: 0,
-      duration: 1,
-      ease: "power2.inOut",
-      onComplete: function () {
-        loading.style.display = "none";
-      }
-    });
-  }
+  tl.to(".loading", {
+    opacity: 0,
+    duration: 1,
+    ease: "power2.inOut",
+    onComplete: function () {
+      loading.style.display = "none";
+    }
+  });
 });
